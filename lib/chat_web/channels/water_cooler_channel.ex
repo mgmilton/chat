d7921f296ecd9c3aedc9ecbd13384070fe973650
mgmilton <matt.milton@gmail.com>
2018-08-16 09:30:54 -0700

    remove authorization function

--- lib/chat_web/channels/water_cooler_channel.ex
+++ lib/chat_web/channels/water_cooler_channel.ex
@@ -2,11 +2,7 @@ defmodule ChatWeb.WaterCoolerChannel do
   use ChatWeb, :channel
 
   def join("water_cooler:lobby", payload, socket) do
-    if authorized?(payload) do
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
+    {:ok, socket}
   end
 
   # Channels can be used in a request/response fashion
@@ -20,10 +16,5 @@ defmodule ChatWeb.WaterCoolerChannel do
   def handle_in("shout", payload, socket) do
     broadcast socket, "shout", payload
     {:noreply, socket}
-  end
-
-  # Add authorization logic here as required.
-  defp authorized?(_payload) do
-    true
   end
 end
